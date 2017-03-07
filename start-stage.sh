3a2a16199e7b1560eb3aa830b075c428db3af702
Gloria Bae and Kris Gonzalez <pair+gloria+kris@samsung.com>
2017-03-07 13:40:55 +0900

    update shell for exception.. PID file..

--- start-stage.sh
+++ start-stage.sh
@@ -1,2 +1,13 @@
-target/universal/stage/bin/nxt_api_proxy &
+#/bin/bash
 
+cd target/universal/stage
+
+if [[ -f RUNNING_PID ]] && ps -p $(cat RUNNING_PID) > /dev/null 2>&1
+then
+   echo "Already Stage Running... $(cat RUNNING_PID)"
+else
+   bin/nxt_api_proxy &
+   echo "Stage Started.."
+fi
+
+exit 0
--- stop-stage.sh
+++ stop-stage.sh
@@ -1,3 +1,14 @@
+#/bin/bash
+
 cd target/universal/stage
-kill -9 `cat RUNNING_PID`                
-rm RUNNING_PID 
+
+if [[ -f RUNNING_PID ]] && ps -p $(cat RUNNING_PID) > /dev/null 2>&1
+then
+   kill $(cat RUNNING_PID)
+   rm RUNNING_PID
+   echo "Stage Stopped.."
+else
+   echo "Not Running...."
+fi
+
+exit 0
